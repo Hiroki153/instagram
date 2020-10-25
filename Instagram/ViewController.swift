96a962458eda37f33b28135505f4e1ed13d1a785
Rascal <namekooyazi@gmail.com>
2020-10-25 16:49:56 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by 仲井宏紀 on 2020/10/25.
+//  Copyright © 2020 hiroki.nakai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
