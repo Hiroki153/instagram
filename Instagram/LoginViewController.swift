f34949a6657665bde87663c14d3c0a3ac22ea3d3
Rascal <namekooyazi@gmail.com>
2020-11-01 22:17:29 +0900

    ログイン画面を実装した

--- Instagram/LoginViewController.swift
+++ Instagram/LoginViewController.swift
@@ -11,7 +11,8 @@ import Firebase
 
 class LoginViewController: UIViewController {
     
-    @IBOutlet weak var mailAdressTextField: UITextField!
+
+    @IBOutlet weak var mailAddressTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var displayNameTextField: UITextField!
     
@@ -21,8 +22,43 @@ class LoginViewController: UIViewController {
     
     //アカウント作成ボタンをタップした時に呼ばれるメソッド
     @IBAction func handleCreateAccountButton(_ sender: Any) {
+        if let address = mailAddressTextField.text, let password = passwordTextField.text, let displayName = displayNameTextField.text {
+
+        // アドレスとパスワードと表示名のいずれかでも入力されていない時は何もしない
+        if address.isEmpty || password.isEmpty || displayName.isEmpty {
+            print("DEBUG_PRINT: 何かが空文字です。")
+            return
+        }
+
+        // アドレスとパスワードでユーザー作成。ユーザー作成に成功すると、自動的にログインする
+        Auth.auth().createUser(withEmail: address, password: password) { authResult, error in
+            if let error = error {
+                // エラーがあったら原因をprintして、returnすることで以降の処理を実行せずに処理を終了する
+                print("DEBUG_PRINT: " + error.localizedDescription)
+                return
+            }
+            print("DEBUG_PRINT: ユーザー作成に成功しました。")
+
+            // 表示名を設定する
+            let user = Auth.auth().currentUser
+            if let user = user {
+                let changeRequest = user.createProfileChangeRequest()
+                changeRequest.displayName = displayName
+                changeRequest.commitChanges { error in
+                    if let error = error {
+                        // プロフィールの更新でエラーが発生
+                        print("DEBUG_PRINT: " + error.localizedDescription)
+                        return
+                    }
+                    print("DEBUG_PRINT: [displayName = \(user.displayName!)]の設定に成功しました。")
+
+                    // 画面を閉じてタブ画面に戻る
+                    self.dismiss(animated: true, completion: nil)
+                }
+            }
+        }
     }
-    
+    }
     
 
     override func viewDidLoad() {
