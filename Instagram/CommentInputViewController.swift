b28b2c676c371eb2b0c25889de1fb00aa69bb3a0
Rascal <namekooyazi@gmail.com>
2020-11-15 19:20:31 +0900

    CommetInputViewControllerを追加

---
+++ Instagram/CommentInputViewController.swift
@@ -0,0 +1,64 @@
+//
+//  CommentInputViewController.swift
+//  Instagram
+//
+//  Created by 仲井宏紀 on 2020/11/15.
+//  Copyright © 2020 hiroki.nakai. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import SVProgressHUD
+
+class CommentInputViewController: UIViewController {
+    
+    @IBOutlet weak var commentField: UITextField!
+    
+    let postData = PostData(document: <#T##QueryDocumentSnapshot#>)
+    
+    //キャンセルボタンをタップした時に呼ばれるメソッド
+    @IBAction func commentCancelButton(_ sender: Any) {
+        //投稿画像一覧画面に戻る
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    //投稿ボタンをタップした時に呼ばれるメソッド
+    @IBAction func commentPostButton(_ sender: Any) {
+        let postData = self.postData
+        // 更新されたコメントを書き込む
+        let postRef = Firestore.firestore().collection(Const.PostPath).document(postData.id)
+        // HUDで投稿処理中の表示を開始
+        SVProgressHUD.show()
+        // FireStoreに投稿データを保存する
+        let name = Auth.auth().currentUser?.displayName
+        let postDic = [
+        "commenteduser": name!,
+        "comment": self.commentField.text!,
+        ] as [String : Any]
+        postRef.setData(postDic)
+        // HUDで投稿完了を表示する
+        SVProgressHUD.showSuccess(withStatus: "投稿しました")
+        // 投稿処理が完了したので先頭画面に戻る
+        UIApplication.shared.windows.first{ $0.isKeyWindow }?.rootViewController?.dismiss(animated: true, completion: nil)
+    }
+        
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //受け渡されたデータの受け取り
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
